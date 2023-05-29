035fae29d6b6759afa06d41e3a4d8437cc49306f
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-29 18:19:04 +0200

    remove comments and params

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -8,32 +8,20 @@ use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        return view('admin.comics.index');
+        $comics = Comic::all();
+        return view('admin.comics.index', compact('comics'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('admin.comics.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $comic = new Comic();
@@ -48,12 +36,7 @@ class ComicController extends Controller
         return to_route('comics.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Comic  $comic
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Comic $comic)
     {
         return view('admin.comics.show', compact('comic'));
