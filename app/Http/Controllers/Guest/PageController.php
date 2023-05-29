98884ab698634ece72c865ffa58589c14ece422c
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-29 17:35:25 +0200

    add comics to page controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $comics = Comic::all();
+        return view('home', compact('comics'));
     }
 }
