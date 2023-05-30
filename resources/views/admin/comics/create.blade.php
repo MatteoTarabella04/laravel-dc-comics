69fc17026b4703962077386d5b03b5d2f4515bd4
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-30 10:04:14 +0200

    add some style to form

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container pt-4">
+        <h1 class="text-center py-2">Add new comic</h1>
         <form action="{{ route('comics.store') }}" method="post">
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" id="title" nmae="title" required>
             </div>
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <input type="text" class="form-control" id="description" name="description" required>
             </div>
@@ -14,24 +15,24 @@
                 <label for="thumb" class="form-label">Image</label>
                 <input class="form-control" type="file" id="thumb" name="thumb">
             </div>
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
                 <input type="number" class="form-control" id="price" name="price" required>
             </div>
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
                 <input type="text" class="form-control" id="series" name="series" required>
             </div>
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale Date</label>
                 <input type="date" class="form-control" id="sale_date" name="sale_date" required>
             </div>
-            <div class="col-md-4">
+            <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
                 <input type="date" class="form-control" id="type" name="type" required>
             </div>
-            <div class="col-12">
-                <button class="btn btn-primary" type="submit">Create</button>
+            <div class="d-flex justify-content-center py-4">
+                <button class="btn w-50 btn-primary" type="submit">Create</button>
             </div>
         </form>
     </div>
