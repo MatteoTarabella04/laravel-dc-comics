2a2aaa63fd971bffbec11b95c3a9a357b4a11d50
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-06-01 13:12:19 +0200

    add modal for delete

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -39,7 +39,7 @@
                             <td scope="row">{{ $comic->id }}</td>
                             <td>{{ $comic->title }}</td>
                             <td><img height="70" src="{{ $comic->thumb }}" alt=""></td>
-                            <td>{{ $comic->price }}</td>
+                            <td>${{ $comic->price }}</td>
                             <td>{{ $comic->series }}</td>
                             <td>{{ $comic->sale_date }}</td>
                             <td>{{ $comic->type }}</td>
@@ -60,12 +60,54 @@
                                             <i class="fas fa-pencil fa-sm fa-fw"></i>
                                         </a>
                                     </button>
-                                    <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
+
+
+
+                                    <!-- Modal trigger button -->
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#modal-{{ $comic->id }}">
+                                        <i class="fas fa-trash fa-sm fa-fw"></i>
+                                    </button>
+
+                                    <!-- Modal Body -->
+                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                    <div class="modal fade" id="modal-{{ $comic->id }}" tabindex="-1"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                        aria-labelledby="modalTitle-{{ $comic->id }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                            role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="modalTitle-{{ $comic->id }}">Delete
+                                                        {{ $comic->title }}</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Do you want to delete {{ $comic->title }}?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Close</button>
+                                                    <form action="{{ route('comics.destroy', $comic->id) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger">Confirm</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+                                    {{-- <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger"><i
                                                 class="fas fa-trash fa-sm fa-fw"></i></button>
-                                    </form>
+                                    </form> --}}
 
                                 </div>
                             </td>
